15477592adce082094a527a22642d78b739703bc
Mike Wynn <mikewynn@mikes-mbp.lan>
2019-04-27 10:21:57 -0700

    fixed formatting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    state = {
+        todos: [
+            {
+                id: uuid.v4(),
+                title: 'Todo 1',
+                completed: false
+            },
+            {
+                id: uuid.v4(),
+                title: 'Todo 2',
+                completed: false
+            },
+            {
+                id: uuid.v4(),
+                title: 'This is a todo 3',
+                completed: false
+            },
+        ]
+    }
+
+    markComplete = (id) => {
+        this.setState({ todos: this.state.todos.map(todo => {
+            if(todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo;
+        }) });
+    }
+
+    delTodo = (id) => {
+        this.setState({ todos: [...this.state.todos.filter(todo => todo.id
+        !== id)] });
+    }
+
+    addTodo = (title) => {
+        const newTodo = {
+            id: uuid.v4(),
+            title,
+            completed: false
+        }
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="container">
+                <Header />
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo} />
+                </div>
+            </div>
+            
+        );
+    }
+};
 
 export default App;
